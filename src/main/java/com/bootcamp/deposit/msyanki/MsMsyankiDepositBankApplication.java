1034eda7b6039d8cf9fcca56cbd2ce8ec9b61cc2
EduCCor <ecordovc@everis.com>
2021-08-31 12:43:31 -0500

    services deposit yanki

---
+++ src/main/java/com/bootcamp/deposit/msyanki/MsMsyankiDepositBankApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.deposit.msyanki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMsyankiDepositBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMsyankiDepositBankApplication.class, args);
+	}
+
+}
